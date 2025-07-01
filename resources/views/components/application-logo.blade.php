b8b78f3de1862ab0a42564128711740fa0499e16
Gari Vivar <garivivar@RLIs-MacBook-Air.local>
2025-07-01 14:17:35 +0800

    fix image

--- resources/views/components/application-logo.blade.php
+++ resources/views/components/application-logo.blade.php
@@ -2,5 +2,5 @@
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet">
 
 <div class="flex items-center">
-     <img style="height:auto; width:450px" src = "{{ asset('/images/es_logo.png') }}" />
+     <img style="height:auto; width:450px" src = "{{ asset('/images/ES_logo.png') }}" />
 </div>
